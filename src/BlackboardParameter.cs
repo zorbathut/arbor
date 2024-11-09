61cd3a308c78e80d39c664118f47096a9358c570
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-11-09 14:42:55 -0600

    Tentative Constant support.

--- src/BlackboardParameter.cs
+++ src/BlackboardParameter.cs
@@ -13,31 +13,55 @@ namespace Arbor
 
     public struct BlackboardParameter<T> : Dec.IRecordable
     {
-        BlackboardIdentifier identifier;
+        BlackboardIdentifier? identifier;
+        T constant;
 
         public static BlackboardParameter<T> Tree(string id)
         {
             return new BlackboardParameter<T> { identifier = new BlackboardIdentifier{ id = id } };
         }
 
+        public static BlackboardParameter<T> Constant(T initial)
+        {
+            return new BlackboardParameter<T> { constant = initial };
+        }
+
         public T Get()
         {
-            return State.Current.Value.BlackboardGet<T>(identifier);
+            if (identifier.HasValue)
+            {
+                return State.Current.Value.BlackboardGet<T>(identifier.Value);
+            }
+            else
+            {
+                return constant;
+            }
         }
 
         public void Set(T value)
         {
-            State.Current.Value.BlackboardSet<T>(identifier, value);
+            if (identifier.HasValue)
+            {
+                State.Current.Value.BlackboardSet<T>(identifier.Value, value);
+            }
+            else
+            {
+                Dbg.Err("Attempted to set a constant blackboard parameter");
+            }
         }
 
         public void RegisterWith(Blackboard blackboard)
         {
-            blackboard.Register(identifier.id, typeof(T));
+            if (identifier.HasValue)
+            {
+                blackboard.Register(identifier.Value.id, typeof(T));
+            }
         }
 
         public void Record(Dec.Recorder recorder)
         {
             recorder.RecordAsThis(ref identifier);
+            recorder.RecordAsThis(ref constant);
         }
     }
 }
